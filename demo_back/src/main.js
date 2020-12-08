f92515e94aa146954270e4ae3ae9d2e4a3fe9a39
xiongyechang <2226791767@qq.com>
2020-12-08 20:29:39 +0800

    创建各种项目工程目录

---
+++ demo_back/src/main.js
@@ -0,0 +1,16 @@
+console.log('hello world');
+
+function _alert (msg) {
+  console.log(msg);
+}
+
+_alert('hello world');
+
+function Person (name, no) {
+  this.name = name;
+  this.no = no;
+}
+
+const person = new Person('熊业昌', '0001');
+
+console.log(person);
---
+++ demo_back/vue.config.js
@@ -0,0 +1,7 @@
+// webpack 魔法注释
+/** @type { import(@vue/cli-service).ProjectOptions } */
+// npx vue-cli-service inspect // 打印整合后的webpack配置
+// npx vue-cli-service > webpack.config.js inspect 把结果输出到文件
+module.export = {
+
+}
