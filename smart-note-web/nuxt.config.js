7cde15ca767277b771a138b829f98d87a1e1eaf6
xiongyechang <2226791767@qq.com>
2020-12-26 19:54:33 +0800

    修改项目名称

--- smart-note-web/nuxt.config.js
+++ smart-note-web/nuxt.config.js
@@ -1,7 +1,7 @@
 export default {
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
-    title: 'code-snippet-web',
+    title: 'smart-note-web',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
